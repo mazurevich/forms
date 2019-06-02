e2ebf225838ba8198267ca704ce25c0bea999dcc
mazurevich <mazyrevich@gmail.com>
2019-06-03 00:02:03 +0300

    formatted some files

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Form, Input } from './components'
 
 function App() {
   const [inputValue1, setInputValue1] = useState('')
-  const [inputValue2, setInputValue2] = useState('')
-  const [inputValue3, setInputValue3] = useState('')
   const inputRef = useRef(null)
 
   return (
@@ -15,28 +13,10 @@ function App() {
           label="My Input 1"
           name="input1"
           id="test1"
+          ref={inputRef}
           value={inputValue1}
           onChange={(value, name) => {
             setInputValue1(value)
-          }}
-        />
-        <Input
-          label="My Input 2"
-          name="input2"
-          id="test2"
-          ref={inputRef}
-          value={inputValue2}
-          onChange={(value, name) => {
-            setInputValue2(value)
-          }}
-        />
-        <Input
-          label="My Input 3"
-          name="input3"
-          id="test3"
-          value={inputValue3}
-          onChange={(value, name) => {
-            setInputValue3(value)
           }}
         />
         <button
